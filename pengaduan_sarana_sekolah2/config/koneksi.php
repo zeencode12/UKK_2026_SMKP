edcb064a4fd7c03b0f3d71e7e833eb0c670a0d25
Zainudin,S.Kom <163313692+zeencode12@users.noreply.github.com>
2026-01-23 09:29:27 +0700

    Delete pengaduan_sarana_sekolah2 directory

--- pengaduan_sarana_sekolah2/config/koneksi.php
+++
@@ -1,4 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost","root","","pengaduan_sarana");
-if(!$conn){ die("Koneksi gagal"); }
-?>
--- pengaduan_sarana_sekolah2/hapus_kategori.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'config/koneksi.php';
-
-$id = $_GET['id'];
-mysqli_query($conn, "DELETE FROM kategori WHERE id_kategori='$id'");
-
-header("location:input_kategori.php");
-?>
