50e9435a3f4227386427d68c1b036b86f48f8d2c
Zainudin,S.Kom <163313692+zeencode12@users.noreply.github.com>
2026-02-01 20:44:04 +0700

    Refactor manage_admin.php for improved structure

--- pengaduan_sarana_sekolah3/manage_admin.php
+++ pengaduan_sarana_sekolah3/manage_admin.php
@@ -20,6 +20,7 @@ if (isset($_GET['cari']) && $_GET['cari'] != "") {
 <head>
 <meta charset="UTF-8">
 <title>Manage Admin</title>
+<link rel="icon" type="image/x-icon" href="image/school.ico">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
 <style>
